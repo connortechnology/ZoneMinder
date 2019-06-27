8cb9eafddf1268b055a976da1c92de74b1a9f44b
Isaac Connor <isaac@zoneminder.com>
2019-06-27 04:12:19 -0400

    add and populate sensor_action_types

--- db/zm_sensor_actions.sql
+++ db/zm_sensor_actions.sql
@@ -16,3 +16,22 @@ CREATE TABLE Sensor_Actions (
 
 INSERT INTO Sensor_Actions (`SensorId`, `MonitorId`, `TypeId`, `MinValue`, `MaxValue`, `Action`) VALUES
 (3, 1, 2, NULL, NULL, 'Preset 1');
+
+CREATE TABLE Sensor_Action_Types (
+  `Id` integer unsigned NOT NULL auto_increment,
+  `TypeId`  varchar(64),
+  `Name`    varchar(64),
+  KEY `Sensor_Action_Types_TypeId_idx` (`TypeId`),
+  PRIMARY KEY (`Id`)
+);
+
+INSERT INTO Sensor_Action_Types (`TypeId`, `Name`) VALUES 
+('01', 'HIGH_VB_ALARM'),
+('02', 'TEMP_ALARM'),
+('03', 'MAG_ALARM'),
+('04', 'WIND_FILTER'),
+('05', 'TREND_FILTER'),
+('06', 'RADAR_FILTER'),
+('07', 'NODE_MISSING_ALARM'),
+('08', 'WIRECUT')
+    ;
