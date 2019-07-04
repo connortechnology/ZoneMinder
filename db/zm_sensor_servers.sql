723730c5f91d94b387163f8c0ecd00b23010e71e
Isaac Connor <isaac@zoneminder.com>
2019-07-04 18:20:27 -0400

    Update default config for Node1.  Add Enabled column

--- db/zm_sensor_servers.sql
+++ db/zm_sensor_servers.sql
@@ -7,7 +7,8 @@ CREATE TABLE Sensor_Servers (
   `Url`    VARCHAR(255),
   `PollFrequency` INTEGER UNSIGNED,
   `Chains`  VARCHAR(255),
+  `Enabled` BOOLEAN NOT NULL DEFAULT TRUE,
   PRIMARY KEY (`Id`)
 );
 
-INSERT INTO Sensor_Servers (`Name`,`Url`,`PollFrequency`) VALUES ('Node1','http://www.texonglobal.com/astatus.xml', 3, '1:Left,2:Right');
+INSERT INTO Sensor_Servers (`Name`,`Url`,`PollFrequency`,`Chains`,`Enabled`) VALUES ('Node1','http://www.texonglobal.com/astatus.xml', 3, '1:Left,2:Right', true);
