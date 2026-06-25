da58a3c8584f76cf628dbc9894739767002948ae
Isaac Connor <isaac@zoneminder.com>
2026-06-25 16:14:34 -0400

    SIDEBAR_FOOTER is not part of the actual menu

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -441,8 +441,6 @@ function buildSidebarMenu() {
               </a>
             </li>
 ';
-  } else if (defined('ZM_SIDEBAR_FOOTER') and ZM_SIDEBAR_FOOTER) {
-    $menu .= ZM_SIDEBAR_FOOTER;
   }
   $menu .= '
           </ul>
